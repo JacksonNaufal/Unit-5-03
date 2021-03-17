3fca9b7a7927bafddc7cd6cf85acb3f824c5df4c
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-17 13:27:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('report').addEventListener('click',reportResults)
+document.getElementById('button').addEventListener('click',button)
+document.getElementById('paragraph').addEventListener('click',paragraph)
+document.getElementById ('para').addEventListener('click',para)
+
+function reportResults () {
+  alert(document.getElementById('input').value)
+}
+
+function button () {
+  document.getElementById('background').style.backgroundColor = 'red'
+  document.getElementById('paragraph').innerHTML = 
+  'Click Mario!!'
+  document.getElementById('paragraph').style.color = 'orange'
+  document.getElementById('paragraph').style.fontSize = '250%'
+}
+
+function para () {
+  document.getElementById('picture').src =
+  'mario.jpg'
+}
