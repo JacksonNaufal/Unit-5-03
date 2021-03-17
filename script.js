261c13753d2b7cc494c6b9cc5cd1f30ba0a55f1d
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-17 13:36:06 +0000

    all errors except some

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.getElementById('report').addEventListener('click',reportResults)
-document.getElementById('button').addEventListener('click',button)
-document.getElementById('paragraph').addEventListener('click',paragraph)
-document.getElementById ('para').addEventListener('click',para)
+document.getElementById('report').addEventListener('click', reportResults)
+document.getElementById('button').addEventListener('click', button)
+document.getElementById('paragraph').addEventListener('click', paragraph)
+document.getElementById('para').addEventListener('click', para)
 
 function reportResults () {
   alert(document.getElementById('input').value)
@@ -9,7 +9,7 @@ function reportResults () {
 
 function button () {
   document.getElementById('background').style.backgroundColor = 'red'
-  document.getElementById('paragraph').innerHTML = 
+  document.getElementById('paragraph').innerHTML =
   'Click Mario!!'
   document.getElementById('paragraph').style.color = 'orange'
   document.getElementById('paragraph').style.fontSize = '250%'
